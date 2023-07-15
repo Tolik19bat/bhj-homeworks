89b705e3cbb58c4dab6645f40a8da44b24ecd739
Batov-Anatoliy <play.hoffa@gmail.com>
2023-07-15 18:50:01 +0300

    new-commit

---
+++ js-features/countdown/task.js
@@ -0,0 +1,8 @@
+const intervalId = setInterval(function () {
+  const timer = document.getElementById('timer');
+  timer.textContent -= 1;
+  if(timer.textContent == 0) {
+  clearInterval(intervalId);
+  alert('Вы победили в конкурсе');
+}
+}, 1000);
