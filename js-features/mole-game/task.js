85b985d2dea33f80e5b6cfccb9c8c2c095cd3bef
Batov-Anatoliy <play.hoffa@gmail.com>
2023-07-19 03:15:55 +0300

    new-commit

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,29 @@
+const hole = document.getElementsByClassName("hole");
+const element = document.getElementsByClassName("hole_has-mole");
+
+function getHole() {
+	const holeUp = document.g
+}
+
+for (let i of hole) {
+	
+	
+	if (i) {
+		console.log(i);
+	}
+}
+
+hole.onclick = getHole;
+// alert(element)
+// function click() {
+// 	alert('Клик');
+// }
+
+// tap.onclick = click;
+
+// function getHole( index ) {
+// 	const element = document.getElementsByClassName('hole_has-mole');
+// 	alert(element)
+
+// }
+// alert(getHole());
