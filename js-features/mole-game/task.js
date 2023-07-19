4c4e5db0da1f0bbeb66a5a929bfd910399251571
Batov-Anatoliy <play.hoffa@gmail.com>
2023-07-19 23:23:24 +0300

    new-commit

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,29 +1,27 @@
 const hole = document.getElementsByClassName("hole");
-const element = document.getElementsByClassName("hole_has-mole");
 
-function getHole() {}
-
-  for (let i of hole) {
-		[i].onclick = function() {
-			alert('клик')
-
-		}
-
-      console.log([i]);
-    
-  }
-
-
-// alert(element)
-// function click() {
-// 	alert('Клик');
-// }
-
-// tap.onclick = click;
-
-// function getHole( index ) {
-// 	const element = document.getElementsByClassName('hole_has-mole');
-// 	alert(element)
-
-// }
-// alert(getHole());
+for (let elementHole of hole) {
+  elementHole.onclick = function () {
+    if (elementHole.className.includes("hole_has-mole")) {
+      const counterDead = document.getElementById("dead");
+      counterDead.textContent = +counterDead.textContent + 1;
+    } else {
+      const counterLost = document.getElementById("lost");
+      counterLost.textContent = +counterLost.textContent + 1;
+    }
+    const counterDead = document.getElementById("dead");
+    if (counterDead.textContent === "11") {
+      counterDead.textContent = 0;
+      const counterLost = document.getElementById("lost");
+      counterLost.textContent = 0;
+      alert("Победа! Вы отличный охотник!");
+    }
+    const counterLost = document.getElementById("lost");
+    if (counterLost.textContent === "6") {
+      counterLost.textContent = "0";
+      const counterDead = document.getElementById("dead");
+      counterDead.textContent = "0";
+      alert("Слишком низкая реакция. Попробуйте ещё разок.");
+    }
+  };
+}
