852b9114f37b65bfe9325e43b2b43fd5024e8525
Batov-Anatoliy <play.hoffa@gmail.com>
2023-07-29 14:07:36 +0300

    new-commit

---
+++ dom/ads/task.js
@@ -0,0 +1,18 @@
+const arrayText = Array.from(document.querySelectorAll(".rotator__case"));
+console.log(arrayText);
+
+let element = 0;
+
+setInterval(() => {
+  arrayText.forEach((el) => {
+    el.classList.remove("rotator__case_active");
+  });
+  arrayText[element].classList.toggle("rotator__case_active");
+  console.log(arrayText[element]);
+
+  element += 1;
+
+  if (element === arrayText.length) {
+    element = 0;
+  }
+}, 1000);
