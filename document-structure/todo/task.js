c0f0e3a6a2285564f0203e4b1421fb78bf23bb1f
Batov-Anatoliy <play.hoffa@gmail.com>
2023-08-14 21:50:11 +0300

    new-commit

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,36 +1,36 @@
 const form = document.getElementById("tasks__form");
-form.type = "submit";
+form.type = "submit";//добавляем форме метод submit
 const text = document.getElementById("task__input");
-text.setAttribute("required", true);
+text.setAttribute("required", true);//добавляем атрибут для инпута
 const tasks = document.getElementById("tasks__list");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   // e.stopPropagation();
 
-  const task = document.createElement("div");
+  const task = document.createElement("div");//создаём див
   tasks.prepend(task);
   task.className = "task";
 
-  const taskTitle = document.createElement("div");
+  const taskTitle = document.createElement("div");//дочерний див
   task.prepend(taskTitle);
   taskTitle.className = "task__title";
   taskTitle.textContent = text.value;
 
-  const taskRemove = document.createElement("a");
+  const taskRemove = document.createElement("a");//ссылка для закрытия задачи
   task.appendChild(taskRemove);
   taskRemove.href = "#";
   taskRemove.className = "task__remove";
   taskRemove.textContent = "x";
 
-	const remove = Array.from(document.getElementsByClassName("task__remove"));
+	const remove = Array.from(document.getElementsByClassName("task__remove"));//массив открытых задач
 
-  for (let index = 0; index < remove.length; index++) {
-      remove[index].addEventListener("click", (e) => {
+  for (let index = 0; index < remove.length; index++) {//цикл
+      remove[index].addEventListener("click", (e) => {//при клике на ссылку
         e.preventDefault();
         // e.stopPropagation();
-        task.remove();
+        task.remove();//удаляем задачу
       });
     }
-  form.reset();
+  form.reset();//при добавлении задачи форма очищается
 });
