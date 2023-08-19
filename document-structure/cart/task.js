a70c6ade08cb581270782c56ef6129d6a72c6b9f
Batov-Anatoliy <play.hoffa@gmail.com>
2023-08-20 01:56:13 +0300

    new-commit

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -17,47 +17,39 @@ function addProducts(event) {
   // console.log(`inCart : ${inCart}`);
 
   if (inCart) {//если товар в корзине есть то...
-    const count = inCart.querySelector(".cart__product-count");//получаем элемент счётчика
-    // console.log(`count : ${count}`);
+    const count = inCart.querySelector(".cart__product-count");//получаем счётчик элемента в корзине
 
     const currentCount = parseInt(count.textContent);//извлекаем цыфры счётчика
-    // console.log(`currentCount : ${currentCount}`)
 
-    const input = element.querySelector(".product__quantity-value");
+    const input = element.querySelector(".product__quantity-value");//нажатый элемент с количеством добовляемого товара
     // console.log(`input : ${input}`)
 
-    const select = parseInt(input.textContent);
-    // console.log(`select : ${select}`);
+    const select = parseInt(input.textContent);//количество нажатого товара в цыфрах
 
-    count.textContent = currentCount + select;
+    count.textContent = currentCount + select;//увеличиваем счётчик в корзине
+
   } else {
-    const productCart = document.createElement("div");
-    // console.log(`productCart : ${productCart}`)
+    const productCart = document.createElement("div");//создаём новый div
 
-    productCart.classList.add("cart__product");
-    productCart.dataset.id = elementId;
+    productCart.classList.add("cart__product");//присваиваем диву класс
+    productCart.dataset.id = elementId;//ставим диву data-id: нажатого элемента
 
-    const productImg = document.createElement("img");
-    // console.log(`productImg : ${productImg}`)
+    const productImg = document.createElement("img");//создаём элемент img
 
-    productImg.classList.add("cart__product-image");
-    productImg.src = img;
-    productCart.appendChild(productImg);
+    productImg.classList.add("cart__product-image");//присваиваем класс
+    productImg.src = img;//добавляем атрибут с источником
+    productCart.appendChild(productImg);//img в div последним дочерним элементом
 
-    const countProduct = document.createElement("div");
-    // console.log(`countProduct : ${countProduct}`)
+    const countProduct = document.createElement("div");//новый div для количества
+    countProduct.classList.add("cart__product-count");//новому диву класс
 
-    countProduct.classList.add("cart__product-count");
+    const input = element.querySelector(".product__quantity-value");//нажатый элемент с количеством добовляемого товара
 
-    const input = element.querySelector(".product__quantity-value");
-    console.log(`input : ${input}`)
+    const select = parseInt(input.textContent);//количество нажатого товара в цыфрах
 
-    const select = parseInt(input.textContent);
-    // console.log(`select : ${select}`)
+    countProduct.textContent = select;//передача колчества товаров в цифрах
 
-    countProduct.textContent = select;
-
-    productCart.appendChild(countProduct);
+    productCart.appendChild(countProduct);//собираем в наш новый div
     cartProducts.appendChild(productCart);
   }
 }
