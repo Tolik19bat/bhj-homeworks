32bae292155cf2b5d7b275be131645b054f102fa
Batov-Anatoliy <play.hoffa@gmail.com>
2023-08-22 21:15:11 +0300

    new-commit

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,35 +1,28 @@
 let products = Array.from(document.querySelectorAll(".product")); //получаем массив продуктов
 let cartProducts = document.querySelector(".cart__products"); //корзина для товаров
 
-function addProducts(event) {
-  //функция создания и добавления товара в корзину
-  const element = event.target.closest(".product"); //получаем нажатый элемент
-
+function addProducts(event) { //функция создания и добавления товара в корзину
+  const element = event.target.closest(".product"); //получаем родителя нажатого элемента
   const elementId = element.dataset.id; //извлекаем из него id
-
   const img = element.querySelector(".product__image").src; //извлекаем из него путь к img
 
-  let inCart = cartProducts.querySelector(
-    //проверяем наличие товара в корзине
+  let inCart = cartProducts.querySelector( //наличие товара в корзине cartProducts
     `.cart__product[data-id="${elementId}"]`
   );
 
-  if (inCart) {
-    //если товар в корзине есть то...
+  if (inCart) {  //если такой товар в корзине есть то...
     const count = inCart.querySelector(".cart__product-count"); //получаем счётчик элемента который в корзине
-
     const currentCount = parseInt(count.textContent); //извлекаем цыфры количества
 
     const input = element.querySelector(".product__quantity-value"); //нажатый элемент с количеством добовляемого товара
-
     const select = parseInt(input.textContent); //количество нажатого товара в цыфрах
 
     count.textContent = currentCount + select; //увеличиваем счётчик в корзине
-  } else {
-    const productCart = document.createElement("div"); //создаём новый div
 
+  } else { // в остальных случаях создаём новый товар
+    const productCart = document.createElement("div"); //создаём новый div
     productCart.classList.add("cart__product"); //присваиваем диву класс
-    productCart.dataset.id = elementId; //ставим диву атрибут data-id: нажатого элемента
+    productCart.dataset.id = elementId; //присваиваем диву атрибут data-id: нажатого элемента
 
     const productImg = document.createElement("img"); //создаём элемент img
     productImg.classList.add("cart__product-image"); //присваиваем класс
@@ -40,10 +33,9 @@ function addProducts(event) {
     countProduct.classList.add("cart__product-count"); //новому диву класс
 
     const input = element.querySelector(".product__quantity-value"); //нажатый элемент с количеством добовляемого товара
+    const countInput = parseInt(input.textContent); //извлекаем количество нажатого товара в цыфрах
 
-    const select = parseInt(input.textContent); //извлекаем количество нажатого товара в цыфрах
-
-    countProduct.textContent = select; //передача колчества товаров в цифрах
+    countProduct.textContent = countInput; //передача колчества товаров в цифрах
 
     productCart.appendChild(countProduct); //добавляем в наш новый div последним дочерним элементом новое количество продукта
     cartProducts.appendChild(productCart); //и всё это добавляем в корзину последним дочерним элементом
@@ -53,7 +45,6 @@ function addProducts(event) {
 products.forEach((element) => {
   //к каждому элементу массива
   const minus = element.querySelector(".product__quantity-control_dec");
-
   const plus = element.querySelector(".product__quantity-control_inc");
 
   minus.addEventListener("click", () => {
@@ -78,6 +69,5 @@ products.forEach((element) => {
   });
 
   const button = element.querySelector(".product__add");
-
   button.addEventListener("click", addProducts); //при нажатии на элемент вызывается коллбэк функция!
 });
--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,31 +2,31 @@ const form = document.getElementById("tasks__form");
 form.type = "submit";//добавляем форме метод submit
 const text = document.getElementById("task__input");
 text.setAttribute("required", true);//добавляем атрибут для инпута
-const tasks = document.getElementById("tasks__list");
+const tasks = document.getElementById("tasks__list");//
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const task = document.createElement("div");//создаём див
-  tasks.prepend(task);
-  task.className = "task";
+  task.className = "task";// присваиваем класс
+  tasks.prepend(task);//вставляем первым дочерним
 
   const taskTitle = document.createElement("div");//дочерний див
-  task.prepend(taskTitle);
-  taskTitle.className = "task__title";
-  taskTitle.textContent = text.value;
+  taskTitle.className = "task__title";// присваиваем класс
+  taskTitle.textContent = text.value;// вставляем текст задачи
+  task.prepend(taskTitle);//вставляем первым дочерним в родителя "task"
 
   const taskRemove = document.createElement("a");//ссылка для закрытия задачи
-  task.appendChild(taskRemove);
-  taskRemove.href = "#";
-  taskRemove.className = "task__remove";
+  taskRemove.href = "#";//присваеваем href
+  taskRemove.className = "task__remove";//присваиваем класс
   taskRemove.textContent = "x";
+  task.appendChild(taskRemove);//вставляем последним дочерним элементом
   form.reset();//при добавлении задачи форма очищается
 });
 
 tasks.addEventListener('click', function (event) {
 	event.preventDefault();
 	const remove = event.target;//получаем нажатый элемент
-	let deleteElement = remove.closest('.task');//получаем ближайшего родителя элемента 
+	let deleteElement = remove.closest('.task');//получаем ближайшего родителя элемента
 	deleteElement.remove();//удаляем родителя
 })
